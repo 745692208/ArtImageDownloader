4b708693bd252e81d69bea525874b9c06ead7d89
745692208 <745692208@qq.com>
2021-05-08 22:38:18 +0800

    1

--- src/app.py
+++ src/app.py
@@ -5,9 +5,10 @@ from tkinter import Tk, Frame, Label, Button, Scrollbar, Text, Entry
 from tkinter import Checkbutton, messagebox, filedialog
 from tkinter import TOP, LEFT, BOTH, X, Y, END, IntVar  # BOTTOM
 # from tkinter import ttk
+import pyperclip
 
 import config
-from core import Core
+from core2 import Core
 
 
 class App(Frame):
@@ -22,25 +23,36 @@ class App(Frame):
         print('browse')
         dir = os.path.normpath(filedialog.askdirectory())
         if dir:
-            config.write_config('config.ini', 'Base', 'save_dir', dir)
+            config.write_config('config.ini', 'Base', 'save_path', dir)
             self.save_path = dir
             self.entry_path.delete(0, END)
             self.entry_path.insert(0, self.save_path)
 
-    def run(self):
-        print('run')
+    def check_dir(self):
         if os.path.exists(self.entry_path.get()):
-            self.core.b_is_create_folder = self.ckbtn_create_folder_var.get()
-            self.core.file_save_dir = self.entry_path.get()
-            self.core.run()
-            print('run2')
-            config.write_config(
-                'config.ini', 'Base', 'ckbtn_create_folder_var',
-                self.ckbtn_create_folder_var.get())
-            config.write_config(
-                'config.ini', 'Base', 'save_dir', self.entry_path.get())
+            return True
         else:
             messagebox.showerror("错误", "请输入正确的文件夹路径！")
+            return False
+
+    def set_param(self):
+        self.core.b_is_create_folder = self.ckbtn_create_folder_var.get()
+        self.core.b_is_down_video = self.ckbtn_down_video_var.get()
+        self.core.save_path = self.entry_path.get()
+        config.write_config('config.ini', 'Base', 'save_path',
+                            self.entry_path.get())
+
+    def get_user_works(self):
+        print('get_user_works ui', pyperclip.paste())
+        if self.check_dir():
+            self.set_param()
+            self.core.get_user_works(pyperclip.paste())
+
+    def get_work(self):
+        print('get_work ui', pyperclip.paste())
+        if self.check_dir():
+            self.set_param()
+            self.core.get_work(pyperclip.paste())
 
     def open_folder(self):
         try:
@@ -52,6 +64,10 @@ class App(Frame):
         config.write_config('config.ini', 'Base', 'ckbtn_create_folder_var',
                             str(self.ckbtn_create_folder_var.get()))
 
+    def ckbtn_down_videor_def(self):
+        config.write_config('config.ini', 'Base', 'ckbtn_down_video_var',
+                            str(self.ckbtn_down_video_var.get()))
+
     def createWidgets(self):
         ''' ======== GUI设置 ======== '''
         self.ckbtn_create_folder_var = IntVar()
@@ -60,6 +76,13 @@ class App(Frame):
                 'config.ini', 'Base', 'ckbtn_create_folder_var')))
         except Exception:
             self.ckbtn_create_folder_var.set(0)
+
+        self.ckbtn_down_video_var = IntVar()
+        try:
+            self.ckbtn_down_video_var.set(int(config.read_config(
+                'config.ini', 'Base', 'ckbtn_down_video_var')))
+        except Exception:
+            self.ckbtn_down_video_var.set(0)
 
         '''控件'''
         self.index1 = Frame(self.root)
@@ -79,10 +102,12 @@ class App(Frame):
             .pack(side=LEFT)
         Button(
             self.index1, text='爬取单个作品',
-            command=lambda: self.executor_ui.submit(self.run)).pack(side=LEFT)
+            command=lambda: self.executor_ui.submit(self.get_work))\
+            .pack(side=LEFT)
         Button(
             self.index1, text='爬取用户',
-            command=lambda: self.executor_ui.submit(self.run)).pack(side=LEFT)
+            command=lambda: self.executor_ui.submit(self.get_user_works))\
+            .pack(side=LEFT)
         self.ckbtn_create_folder = Checkbutton(
             self.index1,
             text='创建文件夹',
@@ -90,6 +115,13 @@ class App(Frame):
             command=self.ckbtn_create_folder_def
             )
         self.ckbtn_create_folder.pack(side=LEFT)
+        self.ckbtn_down_video = Checkbutton(
+            self.index1,
+            text='下载视频',
+            variable=self.ckbtn_down_video_var,
+            command=self.ckbtn_down_videor_def
+            )
+        self.ckbtn_down_video.pack(side=LEFT)
         # Checkbutton(self.index1, text='Is 2D?').pack(side=LEFT)
         Label(self.index2, text='Logs:').pack(side=LEFT)
 
@@ -117,7 +149,7 @@ class App(Frame):
         self.pack()
         try:
             self.save_path = config.read_config(
-                'config.ini', 'Base', 'save_dir')
+                'config.ini', 'Base', 'save_path')
         except Exception:
             self.save_path = ''
         self.createWidgets()
--- src/core.py
+++ src/core.py
@@ -1,85 +1,143 @@
 import os
 import time
 import re
-# 第三方库
-import pyperclip
-import requests #pip install --upgrade urllib3==1.25.2
 from concurrent import futures
 from multiprocessing import cpu_count
+
+import requests   # pip install --upgrade urllib3==1.25.2
+from pytube import YouTube  # https://pytube.io/en/latest/user/install.html
+
 
 class Core:
     def log(self, message):
         time_date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         print('[{}]{}'.format(time_date, message))
-    
+
     def __init__(self, log_print=None):
-        if log_print: #好像是靠这个捕获打印。然后调用app.py里的函数。
+        if log_print:  # 好像是靠这个捕获打印。然后调用app.py里的函数。
             global print
             print = log_print
         max_workers = cpu_count()*4
         self.session = requests.session()
         # 异步加载，避免GUI卡顿
         self.executor = futures.ThreadPoolExecutor(max_workers)
-        #self.executor_video = futures.ThreadPoolExecutor(1)
+        self.executor_video = futures.ThreadPoolExecutor(1)
         self.futures_list = []
         self.b_is_create_folder = True
-        self.file_save_dir = ''    #文件保存路径
-        
-    def run(self):
-        # 复制剪切板内容并正则
-        #print('Code run')
-        clip_content = pyperclip.paste()
-        urls = re.findall(r'image_url(.*?),', clip_content)  # 正则表达式解析网页
-        if len(urls) == 0:
-            self.log('[错误]复制的源码有问题，找不到图片下载地址；')
-            return
+        self.b_is_down_video = False
+        self.save_path = ''    # 文件保存路径
+
+    def get_user_works(self, url):  # 获取用户的所有作品
+        '''
+        :param url, str, 网页地址，比如：https://www.artstation.com/pouria_roshan
+        '''
+        self.log('分析中，请不要进行操作...')
+        username = url.split('/')[-1]
+        data = []
+        page = 0    # 一页最大data数为50，0~49，page为1起步
+        while True:
+            page += 1
+            url = 'https://www.artstation.com/users/{}/projects.json?page={}'\
+                .format(username, page)
+            try:
+                j = self.session.get(url).json()
+            except Exception:
+                self.log('[错误]下载失败，请检查网络设置；')
+                return
+            total_count = int(j['total_count'])
+            data += j['data']
+            if page > total_count / 50:
+                break
+        self.log('分析完毕，作者：{}，共计{}个作品，现进行作品分析...'.format(username, len(data)))
+        for wrok in data:
+            self.get_work(wrok['permalink'])
+
+    def get_work(self, url):
+        '''
+        获取单独作品
+        :param url, str, 网页地址，比如：https://www.artstation.com/artwork/5X9mYA
+        '''
+        self.log('分析中，请不要进行操作...')
+        # 1 获取json数据网址
+        url = 'https://www.artstation.com/projects/{}.json'.format(
+            url.rsplit('/', 1)[1])
+        # 2 获取json资产数据
         try:
-            title = re.findall(r'<title>(.*?)</title>', clip_content)[0]  # 正则表达式解析网页
-            title = re.sub('[/\:*?"<>|]', "", title)    #去除标题特殊符号
-        except:
-            self.log('[错误]复制的源码有问题，找不到标题；')
-            return
-        
-        #url处理
-        for i,url in enumerate(urls):   
-            urls[i] = urls[i].rsplit('"', 1)[0].rsplit('"', 1)[1]
-            urls[i] = urls[i].rsplit('?', 1)[0]
-        
-        # 路径处理
-        if self.b_is_create_folder == True: 
-            path = os.path.join(self.file_save_dir, title)  #'E:\Python\down\' + 'ArtStation - Mankind&#39;.......'
-        else:
-            path = self.file_save_dir   #'E:\Python\down\'
-
-        self.log("[正常]解析完成，共计{}张图片；".format(len(urls)))
-        #print('run:{}'.format(path))
-        self.down_images(urls, path)   # 调用下载图片函数
-
-    def down_images(self, urls, save_path):
-        #print('down_images')
-        if not os.path.exists(save_path) and self.b_is_create_folder == True:   #创建目录
-            os.makedirs(save_path, exist_ok=True)
-        for i, url in enumerate(urls):  #循环下载图片
-            time.sleep(1)
-            self.futures_list.append(self.executor.submit(self.down_image, url, save_path, i))  # 异步加载，避免GUI卡顿
-        futures.wait(self.futures_list)
-        self.log("[正常]下载任务已完成；")
-
-    def down_image(self, url, save_path, index):
-        #print('down_image')
-        self.log('[正常]正在下载第{}张，请稍等；'.format(index+1))
-        try:
-            r = self.session.get(url)  #下载图片
+            j = self.session.get(url).json()
         except Exception:
             self.log('[错误]下载失败，请检查网络设置；')
             return
-        
-        # 路径处理
-        file_name = url.split('/')[-1]  #获取图片名字 gabriel-dias-maia-principal.jpg
-        file_name = file_name.rsplit('.', 1)[0] + '-' + str(index) + '.' + file_name.rsplit('.', 1)[1]  #图片名字加index gabriel-dias-maia-principal-0.jpg
-        path = os.path.join(save_path, file_name)   #保存路径和文件名字合并
-        #print('down_image:{}'.format(path))
-        # 写入文件
+        assets = j['assets']    # 获取资产
+        if self.b_is_create_folder:
+            title = j['title'].strip()   # 获取标题
+            title = re.sub(r'[/\:*?"<>|]', "", title)    # 去除标题特殊符号
+            self.save_path = os.path.join(self.save_path, title)
+            if not os.path.exists(self.save_path):   # 创建目录
+                os.makedirs(self.save_path, exist_ok=True)
+        # 3 资产数据分析
+        self.log('分析完毕，作品：{}，现进行下载...'.format(j['title'].strip()))
+        futures_list = []
+        for i, asset in enumerate(assets):
+            if asset['asset_type'] == 'image':
+                url = asset['image_url']
+                file_name = self.get_file_name(asset['image_url'], i, 1)
+                futures_list.append(
+                    self.executor.submit(self.down_file, url, file_name))
+            if asset['asset_type'] == 'video' and self.b_is_down_video:
+                url = re.findall(r'src="(.*?)"', asset['player_embedded'])[0]
+                futures_list.append(
+                    self.executor.submit(self.down_youtube_video, url))
+            if asset['asset_type'] == 'video_clip' and self.b_is_down_video:
+                url = re.findall(r"src='(.*?)'", asset['player_embedded'])[0]
+                r = self.session.get(url)
+                # 获取可下载的网页地址
+                source_media = re.findall(r'src="(.*?)" type=', r.text)[0]
+                file_name = self.get_file_name(source_media, i, 0)
+                futures_list.append(
+                    self.executor.submit(
+                        self.down_file, source_media, file_name))
+        futures.wait(futures_list)
+        self.log("[正常]下载任务已完成；")
+
+    def get_file_name(self, url, index, has_mask):
+        '''
+        把输入的网页地址转换成名字。
+        :param url, str, 输入网页地址，
+        如：https://cdna.artstation.com/..../gabriel-dias-maia-principal.jpg?1620144337；
+        :param index, int, 尾缀index号；
+        :param has_mask, bool, 网页地址的最后是否有问号，如上面的那个地址所示，去掉问号及其后面的东西；
+        '''
+        file_name = url.split('/')[-1]
+        if has_mask:
+            file_name = file_name.rsplit('?')[0]
+        file_name = file_name.rsplit('.', 1)    # 分割名字和格式 name&jpg
+        file_name = '{}-{}.{}'.format(file_name[0], index, file_name[1])
+        return file_name
+
+    def down_file(self, url, file_name):   # 下载图片
+        '''
+        :param url, str, 输入网页地址，如 https://cdna.artstation.com/p/1.jpg；
+        :param file_name, str, 文件保存名字；
+        '''
+        # self.log('正在下载，地址：{}...'.format(url))
+        r = self.session.get(url)  # 下载图片
+        path = os.path.join(self.save_path, file_name)   # 保存路径和文件名字合并
         with open(path, 'wb') as f:
             f.write(r.content)
-        self.log('[完成]{}；'.format(path))
+        self.log('完成下载，地址：{}'.format(url))
+
+    def down_youtube_video(self, url):   # 下载视频
+        '''
+        :param url, str, 输入网页地址，如https://www.youtube.com/watch?v=3uVvUD-5Vl4；
+        '''
+        # self.log('正在下载，地址：{}...'.format(url))
+        YouTube(url).streams.first().download(self.save_path)
+        self.log('完成下载，地址：{}'.format(url))
+
+
+if __name__ == '__main__':
+    core = Core()
+    # core.get_user_works('https://www.artstation.com/wangchen-cg')
+    core.save_path = r'D:\Python\down\test'
+    # core.b_is_create_folder = False
+    core.get_work('https://www.artstation.com/artwork/5X9mYA')
